897f15d801142341846615a8a1ef2df639e2f735
Rajinikar Billa <billarajinikar@gmail.com>
2025-01-26 22:47:34 +0100

    Update - job listing with the Google ads with the card style

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,7 +4,7 @@
     <div class="container mt-4 mb-5">
         <div class="text-center mb-4">
             <h1 class="display-4">{{$pageHeadding}}</h1>
-            <p class="lead">{{$pageDescription}}       
+            <p class="lead">{{$pageDescription}}</p>
         </div>
         @include('sections.searchbox', ['cities' => $cities])
         <div class="row row-cols-1 row-cols-md-2 g-4">
@@ -31,12 +31,12 @@
                 @if($index === 2 || $index === 6)
                     <div class="col">
                         <div class="card h-100 shadow-sm">
-                            <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="card-body text-center">
                                 <!-- Google Ads Code -->
                                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8349643691635774"
                                     crossorigin="anonymous"></script>
                                 <ins class="adsbygoogle"
-                                    style="display:block"
+                                    style="display:block; width:100%; height:150px;"
                                     data-ad-format="fluid"
                                     data-ad-layout-key="-f6+3e+bp-9k-bu"
                                     data-ad-client="ca-pub-8349643691635774"
@@ -58,7 +58,7 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title" ><a href="{{ $jobInfoUrl }}" style="color: #ff5722;">{{ $jobTitle }}</a></h5>
+                                    <h5 class="card-title"><a href="{{ $jobInfoUrl }}" style="color: #ff5722;">{{ $jobTitle }}</a></h5>
                                     <p class="card-text">{{ $listDesciption }}...</p>
                                     <p class="card-text"><small class="text-muted">{{ $employerName }} - <a href="{{ $cityUrl }}" class="text-decoration-none">{{ $municipality }}</a></small></p>
                                     <a href="{{ $jobInfoUrl }}" class="btn btn-primary btn-sm mt-auto align-self-start">Read More</a>
@@ -74,7 +74,7 @@
                                 data-job-employer-name ="{{$employerName}}"
                                 data-job-municipality ="{{$municipality}}"
                             >
-                                <i class="far fa-heart"></i> <!-- Start with the "far" class -->
+                                <i class="far fa-heart"></i>
                             </a>
                         </span>
                     </div>
@@ -85,25 +85,8 @@
             <nav aria-label="Page navigation">
                 <ul class="pagination">
                     <?php 
-                        $route = ($pageType==='cat') ?'jobList':'jobListCity';
+                        $route = ($pageType === 'cat') ? 'jobList' : 'jobListCity';
                     ?>
-                    <script type="application/ld+json">
-                        {
-                        "@context": "https://schema.org",
-                        "@type": "BreadcrumbList",
-                        "itemListElement": [
-                            @for ($i = 1; $i <= $totalPages; $i++)
-                            {
-                            "@type": "ListItem",
-                            "position": {{ $i }},
-                            "name": "Page {{ $i }}",
-                            "item": "{{ route($route, ['slug' => $slug, 'page' => $i]) }}"
-                            }{{ $i < $totalPages ? ',' : '' }}
-                            @endfor
-                        ]
-                        }
-                    </script>
-
                     @if($page > 1)
                         <li class="page-item">
                             <a rel="prev" class="page-link" href="{{ route($route, ['slug' => $slug, 'page' => $page - 1]) }}" aria-label="Previous">
