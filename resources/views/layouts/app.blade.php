c310731e945b6482c01575778a0acd951b51de7c
Rajinikar Billa <billarajinikar@gmail.com>
2025-01-26 22:50:25 +0100

    Update - job listing with the Google ads with the card style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,19 +110,20 @@ footer i:hover {
                 
 </div>
                     @yield('content')
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8349643691635774"
+                            crossorigin="anonymous"></script>
+                        <!-- HorizontalDipsplayAds -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-8349643691635774"
+                            data-ad-slot="1829880624"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                 </div>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8349643691635774"
-     crossorigin="anonymous"></script>
-<!-- HorizontalDipsplayAds -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8349643691635774"
-     data-ad-slot="1829880624"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                
             </main>
 
             <!-- Footer -->
